62e3a20bb8ac38634fa034092cd5354656945126
apple502j <33279053+apple502j@users.noreply.github.com>
2020-11-21 12:07:12 +0900

    Automatically make a PR

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -13,4 +13,10 @@ if git status | grep -q "git add"; then
     git commit --no-gpg-sign -m "New strings from Transifex"
     git push origin $BRANCH
     echo Pushed as $BRANCH
+    
+    IFS="/"
+    read -ra REPONAME <<< "$GITHUB_REPOSITORY"
+    
+    DEFAULT_BRANCH="master"
+    curl --request POST --url https://api.github.com/repos/$GITHUB_REPOSITORY/pulls --header 'Authorization: Bearer $INPUT_GHTOKEN' --header 'Content-Type: application/json' --data '{"title": "Automated translation update", "head": "${REPO[1]}/$BRANCH", "base": "${REPO[1]}/$DEFAULT_BRANCH", "maintainer_can_modify": true, "body": "Daily translation update via Transifex."}'
 fi
