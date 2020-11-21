e4cabd3322029b93c25c32edbe5b0c56b946161f
apple502j <33279053+apple502j@users.noreply.github.com>
2020-11-21 10:26:13 +0900

    Add GitHub Actions config

---
+++ scripts/commit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd $SA_ROOT
+
+if git status | grep -q "Changes not staged for commit"; then
+    echo New strings available. Pushing to GitHub...
+    BRANCH=tx-$(date +"%Y%m%d%H%M%S")
+    git checkout -b $BRANCH
+    git add _locales/*
+    git add addons-l10n/*
+    git commit --no-gpg-sign -m "New strings from Transifex"
+    git push origin $BRANCH
+    echo Pushed as $BRANCH
+fi
