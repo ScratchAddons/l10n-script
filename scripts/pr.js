5c3c5eccf70bc63683e12912e15ab9637ccf156a
apple502j <33279053+apple502j@users.noreply.github.com>
2020-11-21 12:45:28 +0900

    Make a PR using Node

---
+++ scripts/pr.js
@@ -0,0 +1,31 @@
+import {default as chalk} from "chalk";
+import {Octokit} from "@octokit/rest";
+import {createActionAuth} from "@octokit/auth-action";
+
+const DEFAULT_BRANCH = "master";
+
+if (!process.env.BRANCH) {
+    console.error(chalk`{red ERROR}: BRANCH is not set.`);
+    process.exit(1);
+}
+
+const octokit = new Octokit({
+    authStrategy: createActionAuth,
+    userAgent: "ScratchAddons/l10n-script Pull Request maker",
+    timeZone: "UTC"
+});
+
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+const now = new Date();
+const year = now.getFullYear();
+const month = String(now.getMonth() + 1).padStart(2, "0");
+const date = String(now.getDate()).padStart(2, "0");
+
+await octokit.pulls.create({
+    owner,
+    repo,
+    title: `Translation update: ${year}/${month}/${date}`,
+    head: `${owner}:${process.env.BRANCH}`,
+    base: `${owner}:${DEFAULT_BRANCH}`,
+    body: "Daily translation update (via GitHub Actions)."
+});
