b3a07953940e046a1301c8bdd8080b6cc0be38eb
Hans5958 <hans5958@outlook.com>
2024-12-27 00:16:50 +0700

    Revert "commit on existing PRs" commits
    This reverts commit b348b8378a44c9b07887a2bfdff0a8680c18512d.
    This reverts commit 1c24687383e882cd6e8a039d8d8bfa7635e687fa.
    This reverts commit 4b9a1673c8e591454068b78c35743ab026346406.

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -7,29 +7,14 @@ git config user.name "scratchaddons-bot[bot]"
 if git status | grep -q "git add"; then
     echo New strings available. Pushing to GitHub...
     BRANCH=tx
-
-    git fetch
-
-    if git ls-remote -q --exit-code --heads origin $BRANCH; then
-        BRANCH_EXISTS=true
-        git checkout --track origin/$BRANCH
-    else
-        BRANCH_EXISTS=false
-        git branch -D $BRANCH
-        git checkout -b $BRANCH
-    fi
-
+    git branch -D $BRANCH
+    git push origin --delete $BRANCH
+    git checkout -b $BRANCH
     git add _locales/*
     git add addons-l10n/*
     git commit --no-gpg-sign -m "New strings from Transifex"
-    echo Pushing as $BRANCH
-
-    if [ "$BRANCH_EXISTS" = "false" ]; then
-        echo "Creating a pull request..."
-        git push origin $BRANCH
-        node $GITHUB_ACTION_PATH/scripts/pr.js "$BRANCH"
-    else
-        git push
-        echo "Skipping PR creation"
-    fi
+    git push origin $BRANCH
+    echo Pushed as $BRANCH
+    
+    node $GITHUB_ACTION_PATH/scripts/pr.js "$BRANCH"
 fi
